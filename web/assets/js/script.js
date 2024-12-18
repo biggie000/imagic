eed6ea8d1b6fced4231b348ea10e8a7a7f826cca
Sheldon <sheldon123@gmail.com>
2024-12-18 14:10:34 +1300

    Init commit

---
+++ web/assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function(){
+    $('.slider__reel').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+      });
+  });
