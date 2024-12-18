b972698b5ff780dad2471de64b36e68c422b693f
Sheldon <sheldon123@gmail.com>
2024-12-18 14:41:02 +1300

    nav fix

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,5 +1,59 @@
 
+// Navigation
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navLink = document.querySelector(".nav__link");
 
+if(navToggle && header) {
+  navToggle.addEventListener("click", function(e) {
+    document.body.classList.toggle("nav-overlay");
+
+    // Hide the nav
+    if(header.classList.contains("nav-ready")) {
+      
+      header.classList.toggle("nav-visible");
+      
+      setTimeout(function() {
+        header.classList.toggle("nav-ready");
+      }, 300);
+      
+    // Show the nav
+    } else {
+
+      header.classList.toggle("nav-ready");
+  
+      setTimeout(function() {
+        header.classList.toggle("nav-visible");
+      }, 1);
+    }
+
+    e.preventDefault();
+  });
+}
+
+navLink.addEventListener("click", function(e) {
+    document.body.classList.toggle("nav-overlay");
+
+    // Hide the nav
+    if(header.classList.contains("nav-ready")) {
+      
+      header.classList.toggle("nav-visible");
+      
+      setTimeout(function() {
+        header.classList.toggle("nav-ready");
+      }, 300);
+      
+    // Show the nav
+    } else {
+
+      header.classList.toggle("nav-ready");
+  
+      setTimeout(function() {
+        header.classList.toggle("nav-visible");
+      }, 1);
+    }
+  });
 
 $(document).ready(function(){
     $('.slider__reel').slick({
